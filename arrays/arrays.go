53c129fe6bcfff67ad72e3b3fbadb104e662de6d
nick leslie <nleslie@live.com>
2024-11-06 18:57:49 -0600

    add files

---
+++ arrays/arrays.go
@@ -0,0 +1,12 @@
+package arrays
+
+func Sum(numbers []int) int {
+	sum := 0
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
