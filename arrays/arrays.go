2ce99ed35bae9846b8c37cea57fd83f65f761439
nick l <nleslie@live.com>
2024-11-06 19:39:50 -0600

    Arrays wip

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -2,11 +2,16 @@ package arrays
 
 func Sum(numbers []int) int {
 	sum := 0
-	// for i := 0; i < 5; i++ {
-	// 	sum += numbers[i]
-	// }
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 }
