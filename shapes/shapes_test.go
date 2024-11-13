9d581b3c3cfff4f48e572ea175daed85901bbceb
nick l <nleslie@live.com>
2024-11-12 20:42:54 -0600

    shapes polymmorphism interfaces

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -19,9 +19,17 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	expected := 100.0
-	received := Area(rectangle)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
 
-	checkResults(t, expected, received)
+	for _, tt := range areaTests {
+		received := tt.shape.Area()
+		checkResults(t, tt.want, received)
+	}
 }
