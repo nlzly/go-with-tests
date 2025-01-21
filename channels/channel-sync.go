71f1cb8a9c041ba65d164e9a554a96973be742f5
nick leslie <nleslie@live.com>
2025-01-21 15:06:37 +0100

    more rep with channels

---
+++ channels/channel-sync.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 2)
+	// use a wait group
+	go worker(done)
+	go worker(done)
+
+	<-done
+}
