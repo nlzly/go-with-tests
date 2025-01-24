c189bf2b5f52fa218715a33bf93aa2ef6a5706f8
nick leslie <nleslie@live.com>
2025-01-24 11:17:22 -0600

    added files

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,39 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "postTemplate.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
