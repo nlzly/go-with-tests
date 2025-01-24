d30369e4faa401e6e2bbedb657bca28b51c352be
nick leslie <nleslie@live.com>
2025-01-24 08:45:16 -0600

    finish reading files

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,24 +1,73 @@
 package blogposts
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 	"io/fs"
+	"strings"
 )
 
 type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
 }
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagSeparator         = "Tags: "
+)
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
-
 	if err != nil {
 		return nil, err
 	}
-
 	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
 
-	for range dir {
-		posts = append(posts, Post{})
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	return posts, nil
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
 }
--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/fs"
 	blogposts "learning-go-with-tests/reading-files"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
@@ -15,10 +16,33 @@ func (s StubFailiingFS) Open(name string) (fs.File, error) {
 	return nil, errors.New("always failing")
 }
 
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	t.Run("blog post no error", func(t *testing.T) {
@@ -28,9 +52,13 @@ func TestNewBlogPosts(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		if len(posts) != len(fs) {
-			t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
-		}
+		assertPost(t, posts[0], blogposts.Post{
+			Title:       "Post 1",
+			Description: "Description 1",
+			Tags:        []string{"tdd", "go"},
+			Body: `Hello
+World`,
+		})
 	})
 
 	t.Run("blog post fs always error", func(t *testing.T) {
